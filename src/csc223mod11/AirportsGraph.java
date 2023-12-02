7e9730ebd7d44e11864a9fd6516baf19f8598eeb
Cory <ccd2138@email.vccs.edu>
2023-12-02 16:12:41 -0500

    final

--- src/csc223mod11/AirportsGraph.java
+++ src/csc223mod11/AirportsGraph.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
-public class AirportGraphs {
+public class AirportsGraph {
 
 	public static void main(String[] args) {
 
